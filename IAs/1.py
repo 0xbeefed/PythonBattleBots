c4754063b13fdc271d1844f811008e604fbbfb81
Alpacah <arthcha@gmail.com>
2018-02-25 22:24:31 +0100

    Fix collisions

--- IAs/1.py
+++ IAs/1.py
@@ -7,6 +7,6 @@ pos = lib.getCell(mId)
 
 
 for coord in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
-    if lib.getCellContent(pos[0] + coord[0], pos[1] + coord[1]) != 1:
+    if lib.getCellContent(pos[0] + coord[0], pos[1] + coord[1]) == 0:
         lib.moveOn(pos[0] + coord[0], pos[1] + coord[1])
         break
