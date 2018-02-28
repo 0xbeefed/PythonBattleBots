a55c44acba170f1939157900763ab6c7aaca2338
Alexandre GAULT <alexandre.gault@insa-cvl.fr>
2018-02-28 19:28:51 +0100

    astar in IAs/2.py

--- IAs/2.py
+++ IAs/2.py
@@ -8,47 +8,46 @@ enemyCell = lib.getCell(lib.getEnemyId())
 tab = []
 OBSTACLES = lib.getObstacles()
 
-for obstacle in OBSTACLES:
-    lib.mark(obstacle[0], obstacle[1], 'yellow')
-for y in range(16):
-    tab.append([0]*16)
-    for x in range(16):
-        if lib.getCellContent(x,y):
-            tab[y][x] = 0
-             #OBSTACLES.append([x,y])
+def cellDistance(pos, pos2):
+    return abs(pos[0]-pos2[0])+abs(pos[1]-pos2[1])
+
+
+openList = [pos]
+closedList = []
+parents = {}
+h = True
+        
+while len(openList) != 0 and h:
+    
+    current = openList[0]
+    for tmp in openList:
+        if cellDistance(pos, tmp) + cellDistance(tmp, enemyCell) < cellDistance(pos, current) + cellDistance(current, enemyCell):
+            current = tmp
+    del openList[openList.index(current)]
+    closedList.append(current)
+    
+    if current == enemyCell:
+        break
+
+    for a,b in [[0,1], [0,-1], [1,0], [-1,0]]:
+        X = current[0] + a
+        Y = current[1] + b
+        if lib.getCellContent(X, Y) == -2 or [X,Y] in closedList: continue
+        elif not [X,Y] in openList: openList.append([X,Y]); parents[str([X,Y])] = current
         else:
-            tab[y][x] = 1
-            
-print(tab)
-print('OBSTACLES : ', OBSTACLES)
+            if not str([X,Y]) in parents:
+                parents[str([X,Y])] = current
+        
+        
+tmp = parents[str(enemyCell)]
+path = []
+while tmp != pos:
+    path.append(tmp)
+    tmp = parents[str(tmp)]
+    
+print(*path)
 
-def ldv(pos, pos2):
-    if pos2[0] != pos[0]:
-        for obstacle in OBSTACLES:
-            u = 0
-            d = 0
-            if (obstacle[0] > pos[0]) == (pos2[0] > pos[0]) and (obstacle[1] > pos[1]) == (pos2[1] > pos[1]) and abs(obstacle[0]-pos[0])+abs(obstacle[1]-pos[1]) < abs(pos[0]-pos2[0])+abs(pos[1]-pos2[1]) and obstacle != pos:
-                for a, b in [[0,0], [0,1], [1,0], [1,1]]: # Check that the 4 corners of the obstacle are below or above the line between pos and [x,y]
-                    if obstacle[1]+b > ((pos[1]-pos2[1])/(pos[0]-pos2[0]))*(obstacle[0]+a-pos2[0]-0.5)+pos2[1]+0.5:
-                        u += 1
-                    if obstacle[1]+b < ((pos[1]-pos2[1])/(pos[0]-pos2[0]))*(obstacle[0]+a-pos2[0]-0.5)+pos2[1]+0.5:
-                        d += 1
-                    if obstacle[1]+b == ((pos[1]-pos2[1])/(pos[0]-pos2[0]))*(obstacle[0]+a-pos2[0]-0.5)+pos2[1]+0.5:
-                        u += 1
-                        d += 1
-                if d != 4 and u != 4:
-                    print(pos2[0],pos2[1],obstacle)
-                    return 0
-    else:
-        for obstacle in OBSTACLES:
-            if obstacle[0] == pos2[0] and (min(pos[1], pos2[1]) < obstacle[1] < max(pos[1], pos2[1])):
-                return 0    
-    return 1
+for cell in path:
+    lib.mark(cell[0], cell[1], 'green')
 
 
-for y in range(16):
-    for x in range(16):
-        if not lib.getLineOfSight(pos, [x, y]):
-            lib.mark(x, y, 'red')
-        else:
-            lib.mark(x, y, 'black')
