3e12a9ca4b35cab5326ea1e9da11bf5581043037
Alexandre GAULT <alexandre.gault@insa-cvl.fr>
2018-03-21 21:37:57 +0100

    LoS fixed but does not take into account the aligned cells

--- toast/tostLoS.py
+++ toast/tostLoS.py
@@ -8,22 +8,25 @@ def lineOfSight(start, end, dm):
     if start[1] > end[1]:
         lineOfSight(end, start, dm)
     elif start[0] == end[0]:
+        print('SAME X COORD')
+        """
         pos = start
         tab = [start]
-        print('SAME X COORD')
+        
         while pos != end:
             print(pos, end)
-            pos[1]+dm
+            pos[1] += pos[1]+dm
             tab.append(pos)
         print(tab)
         for i in range(len(tab)):
             cell = tab[i]
             tab[i] = toastCanvas.create_rectangle(cell[0]-20, cell[1]-20, cell[0]+20, cell[1]+20, fill = 'blue')
             toastCanvas.tag_lower(tab[i])
-            root.update()
-            
-    elif start[0] != end[0]:
-
+            root.update()"""
+    elif start[1] == end[1]:
+        print('same Y coord')
+    else:
+        print('in LoS', start, end)
         m = (end[1]-start[1])/(end[0]-start[0])
         p = start[1] - m * start[0]
         print(m)
@@ -48,8 +51,10 @@ def lineOfSight(start, end, dm):
             xb = posB[0]
             yb = posB[1] +dy
             
-                
-            if m*xh+p <= yh:
+            
+
+            
+            if round(m*xh+p, 2) <= yh:
                 posH = [xh, yh]
             else:
                 posH = [xh-dx, yh+dy]
@@ -65,9 +70,10 @@ def lineOfSight(start, end, dm):
             if (posB[0]-20)%40 == 0 and (posB[1]-20)%40 == 0:
                 tab.append(posB)
                   
-            #lines.append(toastCanvas.create_line(tmpH[0], tmpH[1], posH[0], posH[1], fill = 'red'))
-            #lines.append(toastCanvas.create_line(tmpB[0], tmpB[1], posB[0], posB[1], fill = 'green'))
-            #root.update()
+            lines.append(toastCanvas.create_line(tmpH[0], tmpH[1], posH[0], posH[1], fill = 'red'))
+            lines.append(toastCanvas.create_line(tmpB[0], tmpB[1], posB[0], posB[1], fill = 'green'))
+            root.update()
+            #time.sleep(2)
 
         print(tab)
         for i in range(len(tab)):
@@ -94,13 +100,14 @@ def create_lines():
     pos2 = pos1
     while pos2 == pos1:
         pos2 = [random.randint(0,15), random.randint(0,15)]
+
     print("Line of sight between ", pos1, pos2)
 
     pos1[0] = pos1[0] * 40 + 20
     pos1[1] = pos1[1] * 40 + 20
     pos2[0] = pos2[0] * 40 + 20
     pos2[1] = pos2[1] * 40 + 20
-    
+
     toastCanvas.create_line(pos1[0], pos1[1], pos2[0], pos2[1])
     lineOfSight(pos1, pos2, 20)
 
